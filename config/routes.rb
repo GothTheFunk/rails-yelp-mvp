039278785a47c7748cfa78c35a86075c50677446
Gauthier Nadaud <gnadaud@gmail.com>
2022-05-18 15:11:37 +0200

    ajout reviews + review form sur la show resto

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :restaurants do
-    resources :reviews
+  resources :restaurants, except: %i[edit update destroy] do
+    resources :reviews, except: %i[edit update destroy]
   end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
